1ac1303c032cbfd499b500a1c547abd71cb5d8e1
Tao <taozamorano@gmail.com>
2025-06-13 12:13:46 +0100

    update fetch

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -2,7 +2,18 @@ import db
 from datetime import datetime
 import streamlit as st
 from get_hn_item import process_hn_url as fetch
+import pickle
 import re
+
+with open("user_lookup.pkl", "rb") as f:
+    user_lookup = pickle.load(f)
+
+def get_user_features(username, post_time):
+    user = user_lookup.get(username)
+    if not user:
+        return None, None, None
+    age_at_post = (post_time - user["created"]).total_seconds()
+    return user["karma"], user["submitted_count"], age_at_post
 
 default_session_values = [st.session_state.setdefault(k, v) for k, v in {
     'url': None,
@@ -24,6 +35,13 @@ def is_valid_hackernews_url(url):
 
 def get_prediction(url):
     st.session_state.item = fetch(url)
+    # Convert 'time' from int (timestamp) to datetime.datetime
+    if isinstance(st.session_state.item['time'], int):
+        tiem = datetime.fromtimestamp(st.session_state.item['time'])
+    karma, submitted_count, age_at_post = get_user_features(st.session_state.item['user'], tiem)
+    st.session_state.item['karma'] = karma
+    st.session_state.item['submitted_count'] = submitted_count
+    st.session_state.item['age_at_post'] = age_at_post
     return 42
 
 def predict_score_btn():
@@ -36,7 +54,9 @@ def predict_score_btn():
         st.session_state.feedback = "Please enter a HackerNews URL"
         return
 
+
     st.session_state.predicted_score = get_prediction(st.session_state.url)
+
     db.log({
       'timestamp': datetime.now(),
       'item': st.session_state.item,
